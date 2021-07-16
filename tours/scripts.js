9f2be2adcf496993a419f6f5a089a80a44143f65
LHWOOD <hutsonwood@gmail.com>
2021-07-16 08:44:20 -0600

    updated styling in css and created menu

--- tours/scripts.js
+++ tours/scripts.js
@@ -1,1 +1,15 @@
-//start
+//function expression to select elements
+
+const selectElement = (s) => document.querySelector(s);
+
+//open the menu on click
+
+selectElement(".open").addEventListner("click", () => {
+  selectElement(".nav-list").classList.add("active");
+});
+
+//close the menu on click
+
+selectElement(".close").addEventListner("click", () => {
+  selectElement(".nav-list").classList.remove("active");
+});
